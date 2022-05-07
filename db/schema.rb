6ad9aab7ded547c0f2e81d4263704adfe54fe2fe
nekon1021 <cshengli323@gmail.com>
2022-05-07 14:26:00 +0000

    2022-05-07

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220127015751) do
+ActiveRecord::Schema.define(version: 20220505130508) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20220127015751) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "next_day", default: false, null: false
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
@@ -38,8 +39,8 @@ ActiveRecord::Schema.define(version: 20220127015751) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-03-01 23:00:00"
-    t.datetime "work_time", default: "2022-03-02 00:00:00"
+    t.datetime "basic_time", default: "2022-05-03 23:00:00"
+    t.datetime "work_time", default: "2022-05-04 00:00:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
